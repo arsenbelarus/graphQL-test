26acd995f8f5ddb976726b32c700d2bfabf4331d
arsenbelarus <arsenbelarus@gmail.com>
2020-10-01 11:59:38 +0300

    Updates

--- precache-manifest.7d8bb6a214983ea8747c6b3ad59c7740.js
+++ precache-manifest.7d8bb6a214983ea8747c6b3ad59c7740.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e5f05dd33fd296a8a3bd7786b42213d7",
+    "revision": "37217a55432b5d6a4189fb2260d1030a",
     "url": "/graphQL-test/index.html"
   },
   {
-    "revision": "9a932b7b23688e62d0b4",
+    "revision": "ffd51fad6cb8556660e6",
     "url": "/graphQL-test/static/css/main.1b5d3536.chunk.css"
   },
   {
-    "revision": "28cbba8db9326364a270",
-    "url": "/graphQL-test/static/js/2.bc01da99.chunk.js"
+    "revision": "78be6771855a4793eda5",
+    "url": "/graphQL-test/static/js/2.0da38b18.chunk.js"
   },
   {
     "revision": "12cd12724c784f1bdcd4bb0bc8b31cf2",
-    "url": "/graphQL-test/static/js/2.bc01da99.chunk.js.LICENSE.txt"
+    "url": "/graphQL-test/static/js/2.0da38b18.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9a932b7b23688e62d0b4",
-    "url": "/graphQL-test/static/js/main.fb8914a3.chunk.js"
+    "revision": "ffd51fad6cb8556660e6",
+    "url": "/graphQL-test/static/js/main.9ba668ba.chunk.js"
   },
   {
     "revision": "4c5a4ed6721a0692ecad",
