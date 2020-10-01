288ad6df8006edd2532e674612c257ea7bbfa701
arsenbelarus <arsenbelarus@gmail.com>
2020-10-01 11:41:15 +0300

    Updates

---
+++ precache-manifest.d93d55a6498f082c5f1f51f0a84e7588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7116cafc7eee9ca8d40e6f5ef98782b6",
+    "url": "/graphQL-test/index.html"
+  },
+  {
+    "revision": "3d2c5fcd064414146fb8",
+    "url": "/graphQL-test/static/css/main.1b5d3536.chunk.css"
+  },
+  {
+    "revision": "78be6771855a4793eda5",
+    "url": "/graphQL-test/static/js/2.0da38b18.chunk.js"
+  },
+  {
+    "revision": "12cd12724c784f1bdcd4bb0bc8b31cf2",
+    "url": "/graphQL-test/static/js/2.0da38b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2c5fcd064414146fb8",
+    "url": "/graphQL-test/static/js/main.4e168faa.chunk.js"
+  },
+  {
+    "revision": "4c5a4ed6721a0692ecad",
+    "url": "/graphQL-test/static/js/runtime-main.eb3c4771.js"
+  }
+]);
